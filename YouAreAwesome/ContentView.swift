d1e001ee4f3618af471acfff22327a4105f89764
scottmatheson58 <scott@matheson.it>
2026-03-13 17:40:11 +0000

    updad text

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text("You, are Awesome!!!")
+            Text("I am an developer!!!")
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
