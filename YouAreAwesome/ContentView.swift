91a43d0364fd06a1de29a7a67ec6a8b43a858c84
scottmatheson58 <scott@matheson.it>
2026-03-14 11:27:23 +0000

    lession 1.2 addin g buttone

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,17 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message: String = "I am a Prorammer"
     var body: some View {
+        
+        
         
         VStack {
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text("I am an developer!!!")
+            Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
+            
+            
+            Button("Cick Me!") {
+                message="Awesome"
+            
+            }
+            
+            
+
         }
         .padding()
 
