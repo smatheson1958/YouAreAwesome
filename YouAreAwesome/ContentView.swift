d775619eec55165a815d57de185a91bf84f4f9fc
scottmatheson58 <scott@matheson.it>
2026-04-03 17:06:10 +0100

    check

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -30,6 +30,7 @@ struct ContentView: View {
           
            
             Image(imageName)
+             
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
